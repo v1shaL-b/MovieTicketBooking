5205d5a6305d755e9c73186d570de49ca64b7547
Vishal Bhamare <vbhamare11@gmail.com>
2024-03-11 23:06:40 +0530

    test

---
+++ MovieTicketBooking/src/test/java/dev/baron/MovieTicketBooking/MovieTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.baron.MovieTicketBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
