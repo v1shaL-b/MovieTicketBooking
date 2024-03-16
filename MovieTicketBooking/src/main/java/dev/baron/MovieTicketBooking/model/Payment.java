54265424c30a7dcb5624bf5e2f676e3c31a238e7
Vishal Bhamare <vbhamare11@gmail.com>
2024-03-16 11:08:33 +0530

    3

--- MovieTicketBooking/src/main/java/dev/baron/MovieTicketBooking/model/Payment.java
+++ MovieTicketBooking/src/main/java/dev/baron/MovieTicketBooking/model/Payment.java
@@ -3,13 +3,11 @@ package dev.baron.MovieTicketBooking.model;
 
 import dev.baron.MovieTicketBooking.model.constant.PaymentMode;
 import dev.baron.MovieTicketBooking.model.constant.PaymentStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Date;
 import java.time.LocalDateTime;
 
 @Getter
@@ -17,7 +15,8 @@ import java.time.LocalDateTime;
 @Entity(name = "PAYMENT_MASTER")
 public class Payment extends BaseModel{
 
-    private LocalDateTime paymentType;
+    private Date paymentType;
+
     private double amount;
     private String referenceId;
 
